96c21c66f3a6e619461880b66d5581854855bf80
Héctor Martínez Tobar <Hector.MartinezTobar@cerner.com>
2019-12-09 11:41:19 +0000

    Adding secure and non secured code

--- server.php
+++ server.php
@@ -50,7 +50,35 @@ if (isset($_POST['reg_user'])) {
 }
 
 // LOGIN 
+
 if (isset($_POST['login_user'])) {
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  if (empty($username)) {
+  	array_push($errors, "Username is required");
+  }
+  if (empty($password)) {
+  	array_push($errors, "Password is required");
+  }
+
+  if (count($errors) == 0) {
+  	$password = md5($password);
+  	$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+  	$results = mysqli_query($db, $query);
+  	if (mysqli_num_rows($results) == 1) {
+  	  $_SESSION['username'] = $username;
+  	  $_SESSION['success'] = "You are now logged in";
+  	  header('location: index.php');
+  	}else {
+  		array_push($errors, "Wrong username/password combination");
+  	}
+  }
+} 
+
+// secured and escaped code for login 
+
+/* if (isset($_POST['login_user'])) {
   $username = mysqli_real_escape_string($db, $_POST['username']);
   $password = mysqli_real_escape_string($db, $_POST['password']);
 
@@ -73,6 +101,6 @@ if (isset($_POST['login_user'])) {
   		array_push($errors, "Wrong username/password combination");
   	}
   }
-}
+} */
 
 ?>
